32c8f1e584e56206f301df2fe2133aad98eb57af
vikas bansal <vikas.bansal@wynk.in>
2019-11-15 15:27:08 +0530

    Updates

---
+++ precache-manifest.5a37c6d1efe6b14692330406e78cc9b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6361416278a3a299a4b35022308cda38",
+    "url": "/todo-bucket/index.html"
+  },
+  {
+    "revision": "c15a54a381c8a5d4d6e1",
+    "url": "/todo-bucket/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "4f927b3c2f01bd4f4ddc",
+    "url": "/todo-bucket/static/css/main.af66ba29.chunk.css"
+  },
+  {
+    "revision": "c15a54a381c8a5d4d6e1",
+    "url": "/todo-bucket/static/js/2.8cd30ae0.chunk.js"
+  },
+  {
+    "revision": "4f927b3c2f01bd4f4ddc",
+    "url": "/todo-bucket/static/js/main.af8ee657.chunk.js"
+  },
+  {
+    "revision": "9e4f1419e28fa43fd8bd",
+    "url": "/todo-bucket/static/js/runtime-main.8a86bf96.js"
+  }
+]);
